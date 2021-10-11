7da94a3c491a3e932c97f0a173a8f227e03d7d69
X412151 <c.rodriguezvergel@servexternos.gruposantander.com>
2021-10-11 22:42:15 +0200

    get rls state and id :rocket:

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import pysnow
 import requests
-from config import USER,PASSWORD,INSTANCE
+from datetime import datetime, timedelta
+from itertools import chain
+from config import USER, PASSWORD, INSTANCE,RLS
 
 # Create client object
 c = pysnow.Client(instance=INSTANCE, user=USER, password=PASSWORD)
-
-# Define a resource, here we'll use the incident table API
-incident = c.resource(api_path='/v2/table/incident')
-
-# Set the payload
-new_record = {
-    'short_description': 'Pysnow created incident',
-    'description': 'This is awesome'
+number = ""
+rls_text = {
+    "1": "(Nuevo)",
+    "2": "(Certificación)",
+    "14": "(Esperando Aceptación)",
+    "5": "(Pruebas)",
+    "6": "(Evaluar)",
+    "9": "(Implementación)",
+    "10": "(Revisión)",
+    "11": "(Cerrado)"
 }
 
-# Create a new incident record
-result = incident.create(payload=new_record)
+
+def get_status(rls=number):
+    # Define a resource, here we'll use the incident table API
+    incident = c.resource(api_path='/table/rm_release')
+
+    # Query for incidents with state 1
+
+    response = incident.get(query={'number': rls}, stream=True)
+    # Iterate over the result and print out `sys_id` of the matching records.
+    for record in response.all():
+        print(record['sys_id'], rls_text[record['state']])
+        sys_id = record['sys_id']
+        state = record['state']
+        return sys_id, state
+
+
+if __name__ == '__main__':
+    rls_id, rls_state = get_status(rls=RLS)
